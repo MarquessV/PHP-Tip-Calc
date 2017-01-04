a12af36548d1e2b183645f88a87288e909fc8364
Marquess Valdez <marquess@mardebian>
2017-01-04 10:30:32 -0800

    Added custom tip percentage functionality

--- TipCalc.php
+++ TipCalc.php
@@ -6,45 +6,68 @@
 <body>
     <section class="center">
         <h1>Tip Calculator</h1>
-                <!-- Load previous values and error check or enter defaults --!>
                 <?php
                 if(isset($_POST['subtotal'])) {
                     $subtotal=$_POST['subtotal'];
-                    $tipperc=$_POST['tipperc'];
-                    if($subtotal <= 0)
-                            $suberror=True;
-                } else {
+                    if($subtotal <= 0) {
+                        $suberror=True;
+                    }
+                    if(isset($_POST['usecustom'])){
+                        $tipperc=0;
+                        $customtip=$_POST['customtip'];
+                        if($customtip <= 0) {
+                            $tiperror=True;
+                        } 
+                        else {
+                            $tipperc=$_POST['tipperc'];
+                        }  
+                    }
+                } 
+                else {
                     $subtotal='';
-                    $tipperc=.20;
+                    $tipperc=20; 
+                    $customtip='';
                 }
                 ?>
-                <!-- Generate form  --!>
                 <form action="" method="post">
-                    <span <?php if($suberror==True) echo "class=\"error\""?>>Bill subtotal: $ <input type="text" name="subtotal" value="<?php echo $subtotal; ?>"> </span> <br>
+                    <span <?php if($suberror==True) echo "class=\"error\""?>>Bill subtotal: $ <input type="text" name="subtotal" value="<?php echo $subtotal ?>"> </span> <br>
                     Tip percentage:
                     <?php
                     for($i=10; $i <= 20; $i+=5) {
                         if($tipperc==".$i") {
-                            echo "<input type=\"radio\" name=\"tipperc\" value=\".$i\" checked>  $i%\n";
+                            echo "<input type=\"radio\" name=\"tipperc\" value=\"$i\" checked>  $i%\n";
                         } else {
-                            echo "<input type=\"radio\" name=\"tipperc\" value=\".$i\">  $i%\n";
+                            echo "<input type=\"radio\" name=\"tipperc\" value=\"$i\">  $i%\n";
                         }
                     }
                     ?>
                     <br>
+                    <span class="<?php if($tiperror==True) echo "error"?>">
+                    Custom: <input type="radio" name="usecustom" value="" <?php if(isset($_POST['usecustom'])) echo "checked" ?>> 
+                    </span>
+                    <input type="text" name="customtip" value="<?php echo $customtip ?>" style="width:50px;" >% <br>
                     <input type="submit" value="Submit">
                 </form>
-                <!-- Validate, then perform calculations and display them --!>
                 <?php
                 if(isset($_POST['subtotal'])){
                     $subtotal=$_POST['subtotal'];
-                    if($subtotal <= 0)
-                            exit("Enter a subtotal greater than 0");
-                $tipperc=$_POST['tipperc'];
-                $tipamt=$subtotal*$tipperc;
-                $total=$subtotal+$tipamt;
-                echo "Tip: \$$tipamt<br>";
-                echo "Total: \$$total";
+                    if($subtotal <= 0) {
+                        exit("Enter a subtotal greater than 0");
+                    }
+                    if(isset($_POST['usecustom'])){
+                        $customtip=$_POST['customtip'];
+                        if($customtip <= 0) {
+                            exit("Enter a tip percentage greater than 0");
+                        }
+                        $tipperc=$_POST['customtip'];
+                    }
+                    else {
+                        $tipperc=$_POST['tipperc'];
+                    }
+                    $tipamt=$subtotal*($tipperc/100);
+                    $total=$subtotal+$tipamt;
+                    echo "Tip: \$$tipamt<br>";
+                    echo "Total: \$$total";
                 }
                 ?>
     </section>
