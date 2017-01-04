67747b9af29e839c9ea1432d5d5f91fc680c36ed
Marquess Valdez <marquess@mardebian>
2017-01-04 11:09:58 -0800

    Added split check support, changed custom tip from radio to checkbox, fixed result formatting to have two decimals.

--- TipCalc.php
+++ TipCalc.php
@@ -7,26 +7,32 @@
     <section class="center">
         <h1>Tip Calculator</h1>
                 <?php
-                if(isset($_POST['subtotal'])) {
+                if(isset($_POST['subtotal'])){
                     $subtotal=$_POST['subtotal'];
                     if($subtotal <= 0) {
                         $suberror=True;
                     }
+                    $customtip=$_POST['customtip'];
                     if(isset($_POST['usecustom'])){
-                        $tipperc=0;
-                        $customtip=$_POST['customtip'];
                         if($customtip <= 0) {
                             $tiperror=True;
                         } 
-                        else {
-                            $tipperc=$_POST['tipperc'];
-                        }  
+                    }
+                    $tipperc=$_POST['tipperc'];
+                    if(isset($_POST['splitnum'])){
+                        $splitnum=$_POST['splitnum'];
+                        if($splitnum <= 0) {
+                            $spliterror=True; 
+                        }
+                    } else {
+                        $splitnum=1; 
                     }
                 } 
                 else {
                     $subtotal='';
                     $tipperc=20; 
                     $customtip='';
+                    $splitnum=1;
                 }
                 ?>
                 <form action="" method="post">
@@ -34,7 +40,7 @@
                     Tip percentage:
                     <?php
                     for($i=10; $i <= 20; $i+=5) {
-                        if($tipperc==".$i") {
+                        if($tipperc=="$i" && !isset($_POST['usecustom'])) {
                             echo "<input type=\"radio\" name=\"tipperc\" value=\"$i\" checked>  $i%\n";
                         } else {
                             echo "<input type=\"radio\" name=\"tipperc\" value=\"$i\">  $i%\n";
@@ -43,9 +49,12 @@
                     ?>
                     <br>
                     <span class="<?php if($tiperror==True) echo "error"?>">
-                    Custom: <input type="radio" name="usecustom" value="" <?php if(isset($_POST['usecustom'])) echo "checked" ?>> 
+                    Custom: <input type="checkbox" name="usecustom" value="" <?php if(isset($_POST['usecustom'])) echo "checked" ?>> 
                     </span>
-                    <input type="text" name="customtip" value="<?php echo $customtip ?>" style="width:50px;" >% <br>
+                    <input type="text" name="customtip" value="<?php echo $customtip ?>" style="width:25px" >% <br>
+                    <span class="<?php if($spliterror==True) echo "error" ?>">
+                    Split: <input type="text" name="splitnum" value="<?php echo $splitnum ?>" style="width:25px" > person(s) <br>
+                    </span>
                     <input type="submit" value="Submit">
                 </form>
                 <?php
@@ -54,8 +63,9 @@
                     if($subtotal <= 0) {
                         exit("Enter a subtotal greater than 0");
                     }
+                    $customtip=$_POST['customtip']; 
                     if(isset($_POST['usecustom'])){
-                        $customtip=$_POST['customtip'];
+                        $usecustom=True;
                         if($customtip <= 0) {
                             exit("Enter a tip percentage greater than 0");
                         }
@@ -66,8 +76,15 @@
                     }
                     $tipamt=$subtotal*($tipperc/100);
                     $total=$subtotal+$tipamt;
-                    echo "Tip: \$$tipamt<br>";
-                    echo "Total: \$$total";
+                    $splitnum=$_POST['splitnum'];
+                    if($splitnum > 1) {
+                            $splittipamt = $tipamt/$splitnum;
+                            $splittotal = $total/$splitnum;
+                            echo "Tip each: \$", number_format((float)$splittipamt, 2, '.', ''), "<br>";
+                            echo "Total each: \$", number_format((float)$splittotal, 2, '.', ''), "<br>";
+                    }
+                    echo "Tip: \$", number_format((float)$tipamt, 2, '.', ''), "<br>";
+                    echo "Total: \$", number_format((float)$total, 2, '.', '');
                 }
                 ?>
     </section>
